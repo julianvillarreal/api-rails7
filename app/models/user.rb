94b8787c01533732340c179fe96892d07bb6a411
Julian Villarreal <julianvillarrealsierra@gmail.com>
2022-05-04 17:00:20 -0500

    validacion del campo nickname para que sea unico

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@
 
 # Modelo para el objeto User
 class User < ApplicationRecord
-  validates :name, :email, :phone, :age, presence: true
+  validates :name, :email, :phone, :age, presence: true # Valida la presencia del campo
+  validates :nickname, uniqueness: true # Valida que el nickname sea unico
 
-  before_create :create_nickname
+  before_validation :create_nickname, on: :create # Crea el nickname
 
   private
 
+  # Metodo para crear nickname segun el email del usuario
   def create_nickname
     self.nickname = email.split('@')[0]
   end
