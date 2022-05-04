5cce4deb18922fc9ba997f3c4949c6cdf4832aaf
Julian Villarreal <julianvillarrealsierra@gmail.com>
2022-05-04 16:43:11 -0500

    creacion de validaciones de campos obligatorios y creacion de nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
+# Modelo para el objeto User
 class User < ApplicationRecord
+  validates :name, :email, :phone, :age, presence: true
+
+  before_create :create_nickname
+
+  private
+
+  def create_nickname
+    self.nickname = email.split('@')[0]
+  end
 end
